09f815ac87238b0ba926f2f3c276e627c4406b6c
Eugene Chau <eugene.chau01@gmail.com>
2017-06-04 19:18:32 -0700

    Update RTeleportCommand.java
    Sends a message to the player if they
    provide command arguments.

--- src/ca/echau/myfirstplugin/Commands/RTeleportCommand.java
+++ src/ca/echau/myfirstplugin/Commands/RTeleportCommand.java
@@ -17,19 +17,23 @@ public class RTeleportCommand implements CommandExecutor {
 			final Player player = (Player) theSender;
 			if (commandLabel.equalsIgnoreCase("rteleport") || commandLabel.equalsIgnoreCase("rtp")) { //If player typed in "/rteleport"
 				if (player.hasPermission("myfirstplugin.rteleport")) { //Check if the player has perms:
-					final int randomX = (int) (Math.random() * 20000 - 10000); //x coordinate = A random integer from -10000 to 10000
-					final int randomZ = (int) (Math.random() * 20000 - 10000); //z coordinate = A random integer from -10000 to 10000
-					final int randomY = player.getWorld().getHighestBlockYAt(randomX, randomZ); //y coordinate = The highest block at the x and z coordinates. Guarantees that the player doesn't teleport in the ground.
-					final Location randomLocation = new Location(player.getWorld(), randomX, randomY, randomZ);
-					final Material blockUnderneath = randomLocation.getBlock().getRelative(BlockFace.DOWN).getType();	//Gets the block that the player will tp to, then gets the block directly beneath it.
-					if (blockUnderneath == Material.STATIONARY_WATER || blockUnderneath == Material.STATIONARY_LAVA) { //If the block underneath the player is water or lava
-						player.getPlayer().getServer().dispatchCommand(player, "rtp");	//Forces the player to run the command again.
-					} else {	//If the block underneath is not water or lava, tp the player:
-						player.sendMessage(ChatColor.GOLD + "Teleporting..."); //Sends message.
-						player.teleport(randomLocation); //Teleports the player.
-						player.sendMessage(ChatColor.GOLD + "You were randomly teleported to X: " + ChatColor.RED + Integer.toString(randomX) + ChatColor.GOLD
-								+ ", Y: " + ChatColor.RED + Integer.toString(randomY) + ChatColor.GOLD
-								+ ", Z: " + ChatColor.RED + Integer.toString(randomZ)); //Sends success message.
+					if (args.length == 0) {
+						final int randomX = (int) (Math.random() * 20000 - 10000); //x coordinate = A random integer from -10000 to 10000
+						final int randomZ = (int) (Math.random() * 20000 - 10000); //z coordinate = A random integer from -10000 to 10000
+						final int randomY = player.getWorld().getHighestBlockYAt(randomX, randomZ); //y coordinate = The highest block at the x and z coordinates. Guarantees that the player doesn't teleport in the ground.
+						final Location randomLocation = new Location(player.getWorld(), randomX, randomY, randomZ);
+						final Material blockUnderneath = randomLocation.getBlock().getRelative(BlockFace.DOWN).getType();	//Gets the block that the player will tp to, then gets the block directly beneath it.
+						if (blockUnderneath == Material.STATIONARY_WATER || blockUnderneath == Material.STATIONARY_LAVA) { //If the block underneath the player is water or lava
+							player.getPlayer().getServer().dispatchCommand(player, "rtp");	//Forces the player to run the command again.
+						} else {	//If the block underneath is not water or lava, tp the player:
+							player.sendMessage(ChatColor.GOLD + "Teleporting..."); //Sends message.
+							player.teleport(randomLocation); //Teleports the player.
+							player.sendMessage(ChatColor.GOLD + "You were randomly teleported to X: " + ChatColor.RED + Integer.toString(randomX) + ChatColor.GOLD
+									+ ", Y: " + ChatColor.RED + Integer.toString(randomY) + ChatColor.GOLD
+									+ ", Z: " + ChatColor.RED + Integer.toString(randomZ)); //Sends success message.
+						}
+					} else {
+						player.sendMessage(ChatColor.RED + "Invalid arguments! Command usage: " + ChatColor.GOLD + "/rteleport");
 					}
 				} else {
 					player.sendMessage(ChatColor.RED + "You don't have permission to use this command!"); //Sends failure.
